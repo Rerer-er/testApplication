6a896eff84db8acf68bd7ef553c9a863c4543e06
Rerer-er <19it2.pralich.a@pdu.by>
2021-07-13 16:55:13 +0300

    All 3 controller and pagging and another changes

--- TestApplication/Entities/RequestFeatures/ProductParameters.cs
+++ TestApplication/Entities/RequestFeatures/ProductParameters.cs
@@ -6,9 +6,12 @@ namespace Entities.RequestFeatures
 {
     public class ProductParameters : RequestParameters
     {
+        //public string Currency { get; set; } = "RUB";
         public decimal MinPrice { get; set; } = 0;
         public decimal MaxPrice { get; set; } = decimal.MaxValue;
+
         public bool ValidAgeRange => MaxPrice > MinPrice;
 
     }
+    
 }
