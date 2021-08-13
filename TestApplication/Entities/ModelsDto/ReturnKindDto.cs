91215a22631bf6530f9b25587c4196492e8659de
Rerer-er <19it2.pralich.a@pdu.by>
2021-08-13 15:12:41 +0300

    Delete

--- TestApplication/Entities/ModelsDto/ReturnKindDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Entities.ModelsDto
-{
-    public class ReturnKindDto
-    {
-        public int KindId { get; set; }
-        public string Name { get; set; }
-        public string About { get; set; }
-    }
-}
--- TestApplication/Entities/ModelsDto/ReturnShipperDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Entities.ModelsDto
-{
-    public class ReturnShipperDto
-    {
-        public int ShipperId { get; set; }
-        public string Name { get; set; }
-    }
-}
