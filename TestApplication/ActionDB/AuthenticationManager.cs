64c09111903cee51da5f86e51fa0b543c16e8707
Rerer-er <19it2.pralich.a@pdu.by>
2021-08-20 16:26:12 +0300

    adddddd

--- TestApplication/ActionDB/AuthenticationManager.cs
+++ TestApplication/ActionDB/AuthenticationManager.cs
@@ -67,7 +67,8 @@ namespace ActionDB
             issuer: jwtSettings.GetSection("validIssuer").Value,
             audience: jwtSettings.GetSection("validAudience").Value,
             claims: claims,
-            expires: DateTime.Now.AddDays(14));
+            expires: DateTime.Now.AddDays(14), 
+            signingCredentials: signingCredentials);
             // expires: DateTime.Now.AddMinutes(Convert.ToDouble(jwtSettings.GetSection("expires").Value)), signingCredentials: signingCredentials);
             return tokenOptions;
         }
